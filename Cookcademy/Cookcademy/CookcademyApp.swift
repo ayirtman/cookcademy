4c5820869e681a4e294f6b9c957420a436bec30a
Batu Ayirtman <batuayirtman@gmail.com>
2023-05-07 11:01:58 +0200

    First commit

---
+++ Cookcademy/Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by Batuhan Ayırtman on 07.05.23.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
